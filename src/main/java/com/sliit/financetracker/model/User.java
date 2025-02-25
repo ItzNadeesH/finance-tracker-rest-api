7b5e4cb2c4970f5deb406a95f7252a59bd7c33c9
Nadeesh <spnadeeshnimsara@gmail.com>
2025-02-25 19:49:24 +0530

    Update user model

--- src/main/java/com/sliit/financetracker/model/User.java
+++ src/main/java/com/sliit/financetracker/model/User.java
@@ -19,11 +19,37 @@ public class User {
 
     private String role;
 
-    public User(String id, String username, String email, String password, String role) {
-        this.id = id;
+    public User() { }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
         this.role = role;
     }
 }
