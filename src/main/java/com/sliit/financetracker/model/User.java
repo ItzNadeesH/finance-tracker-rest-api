9307aac1e0ea63e8ff7ad72a6ef1e191ffb7a5cf
Nadeesh <spnadeeshnimsara@gmail.com>
2025-02-25 12:52:40 +0530

    Create user model

---
+++ src/main/java/com/sliit/financetracker/model/User.java
@@ -0,0 +1,29 @@
+package com.sliit.financetracker.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String username;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    public User(String id, String username, String email, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
