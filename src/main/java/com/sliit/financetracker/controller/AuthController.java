097c81771dff42458153c6c7ddf153bfa1549b81
Nadeesh <spnadeeshnimsara@gmail.com>
2025-02-26 22:01:14 +0530

    Create login api

--- src/main/java/com/sliit/financetracker/controller/AuthController.java
+++ src/main/java/com/sliit/financetracker/controller/AuthController.java
@@ -2,26 +2,48 @@ package com.sliit.financetracker.controller;
 
 import com.sliit.financetracker.model.User;
 import com.sliit.financetracker.service.AuthService;
+import com.sliit.financetracker.service.JwtService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
 
     private final AuthService authService;
+    private final JwtService jwtService;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, JwtService jwtService) {
         this.authService = authService;
+        this.jwtService = jwtService;
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody @Valid User user) {
+    public ResponseEntity<?> signup(@RequestBody @Valid User user) {
         try {
-            authService.createUser(user.getUsername(), user.getEmail(), user.getPassword(), user.getRole());
+            authService.createUser(user.getUsername(), user.getEmail(), user.getPassword());
             return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully!");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        try{
+            User authenticatedUser = authService.authenticate(user.getEmail(), user.getPassword());
+            String jwtToken = jwtService.generateToken(authenticatedUser);
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("token", jwtToken);
+            response.put("expiresIn", jwtService.getExpirationTime());
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
