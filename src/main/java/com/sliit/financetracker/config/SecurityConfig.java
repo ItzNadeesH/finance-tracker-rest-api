7262d70eaacbf34ea9593d31776d6e0b3ecade78
Nadeesh <spnadeeshnimsara@gmail.com>
2025-02-26 11:18:11 +0530

    Update security filter chain

--- src/main/java/com/sliit/financetracker/config/SecurityConfig.java
+++ src/main/java/com/sliit/financetracker/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.sliit.financetracker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,8 +18,11 @@ public class SecurityConfig {
 
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize  -> authorize
-                .requestMatchers("/auth/signup").permitAll()
-                .anyRequest().authenticated());
+                        .requestMatchers("api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
