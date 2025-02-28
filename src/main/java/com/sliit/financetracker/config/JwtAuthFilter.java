ce4201636453d3bba6e0f03fc0a800e8704151e9
Nadeesh <spnadeeshnimsara@gmail.com>
2025-02-28 21:01:41 +0530

    Refactor code

--- src/main/java/com/sliit/financetracker/config/JwtAuthFilter.java
+++ src/main/java/com/sliit/financetracker/config/JwtAuthFilter.java
@@ -46,8 +46,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-             filterChain.doFilter(request, response);
-             return;
+            filterChain.doFilter(request, response);
+            return;
         }
 
         try {
