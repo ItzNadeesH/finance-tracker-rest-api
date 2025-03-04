3fd6773db260157828ce306f9a82fe9655a5e848
Nadeesh <spnadeeshnimsara@gmail.com>
2025-03-05 02:15:43 +0530

    Create a service to generate financial report according to filters

--- src/main/java/com/sliit/financetracker/repository/TransactionRepository.java
+++ src/main/java/com/sliit/financetracker/repository/TransactionRepository.java
@@ -3,10 +3,12 @@ package com.sliit.financetracker.repository;
 import com.sliit.financetracker.model.Transaction;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
     List<Transaction> findByUserId(String userId);
     Optional<Transaction> findByIdAndUserId(String id, String userId);
+    List<Transaction> findByUserIdAndDateBetween(String userId, LocalDate startDate, LocalDate endDate);
 }
