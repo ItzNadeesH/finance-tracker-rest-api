c1250b56e0525f1b9ba0417d0607ce1c912d50ca
Nadeesh <spnadeeshnimsara@gmail.com>
2025-03-04 22:59:19 +0530

    Refactor budget model

--- src/main/java/com/sliit/financetracker/model/Budget.java
+++ src/main/java/com/sliit/financetracker/model/Budget.java
@@ -1,7 +1,6 @@
 package com.sliit.financetracker.model;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import org.springframework.data.annotation.Id;
