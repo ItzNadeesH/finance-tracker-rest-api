e93965eaf4a3b3380aff9444e013b69b6e384401
Nadeesh <spnadeeshnimsara@gmail.com>
2025-03-05 14:02:53 +0530

    Create notification model

---
+++ src/main/java/com/sliit/financetracker/model/Notification.java
@@ -0,0 +1,79 @@
+package com.sliit.financetracker.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "notifications")
+public class Notification {
+
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String budgetId;
+
+    private String message;
+
+    private boolean isRead;
+
+    private LocalDateTime sendAt;
+
+    public Notification(String userId, String budgetId, String message, LocalDateTime sendAt) {
+        this.userId = userId;
+        this.budgetId = budgetId;
+        this.message = message;
+        this.sendAt = sendAt;
+        this.isRead = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getBudgetId() {
+        return budgetId;
+    }
+
+    public void setBudgetId(String budgetId) {
+        this.budgetId = budgetId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+
+    public LocalDateTime getSendAt() {
+        return sendAt;
+    }
+
+    public void setSendAt(LocalDateTime sendAt) {
+        this.sendAt = sendAt;
+    }
+}
